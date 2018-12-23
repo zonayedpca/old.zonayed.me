ef6e1af57f19cd4868c8e684168d63ce9e516618
Zonayed Ahmed <zonayedpca@yahoo.com>
2018-12-23 19:18:37 +0600

    ghpages

--- src/reducers/index.js
+++
@@ -1,6 +0,0 @@
-import {combineReducers} from 'redux';
-import ReducerAllData from './reducer-allData';
-
-export default combineReducers({
-  ReducerAllData
-});
--- src/reducers/reducer-allData.js
+++
@@ -1,8 +0,0 @@
-export default function(state = null, action) {
-  switch (action.type) {
-    case 'All_DATA':
-      return action.payload.data
-    default:
-      return state;
-  }
-}
